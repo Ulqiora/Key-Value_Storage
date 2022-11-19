4f7c4a03a8c9c9acd36173ec44b7c2ac04fb904b
jcraster <jcraster@student.21-school.ru>
2022-11-19 17:26:21 +0700

    start insert for node

--- src/Model/BTree/BTree.h
+++ src/Model/BTree/BTree.h
@@ -4,21 +4,20 @@
 namespace s21 {
 template <typename Key, typename Value>
 class Btree : public IDataOperations<Key, Value> {
-   private:
-    const static int MAX_ORDER_OF_NODE = 10000;
-    int level = 0;
-    std::shared_ptr<NodeBtree<Key, Value>> root;
-
-   public:
+    //           OVERRIDE IDATAOPERATION METHODS
+ public:
     void SET(const Key& key, const Value& value) final {
-        NodeBtree<Key, Value>* curr = &(*root);
-        if (isEmpty()) {
-            curr = new NodeBtree<Key, Value>(level,TypeOfNode::ROOT);
-            curr->simpleInsert(key,value);
-        } else if (root->getSizeKeyValues() != level) {
-            root->
-            // auto found = std::find_if(currNode->keyValue.begin(),
-            // currNode->keyValue.end(),findFirstLarger);
+        if (rootIsNull()) {
+            root = new NodeBtree<Key, Value>(level, TypeOfNode::LEAF);
+            root.
+        } else {
+            if (root->isCompleted()) {
+                NodeBtree<Key,Value>* tmpNode = new NodeBtree<Key, Value>(level, TypeOfNode::ROOT);
+                tmpNode
+                tmpNode->insert(root);
+            } else {
+                root->insert(key, value);
+            }
         }
     }
     // Value GET(const Key&) final {}
@@ -32,21 +31,26 @@ class Btree : public IDataOperations<Key, Value> {
     // void SHOWALL() final {}
     // void UPLOAD(const std::string&) final {}
     // void EXPORT(const std::string&) final {}
-
-   public:
+    //            CONSTRUCTORS AND DESTRUCTORS
+ public:
     Btree() = delete;
     Btree(int valueOfOrder) {
-        if (valueOfOrder < 2) throw std::invalid_argument("Unacceptable value for the BTree order!");
+        if (valueOfOrder < MIN_ORDER_OF_NODE || valueOfOrder > MAX_ORDER_OF_NODE)
+            throw std::invalid_argument("Unacceptable value for the BTree order!");
         level = valueOfOrder;
     }
     Btree(const Btree&) = delete;
     Btree(Btree&&) = delete;
     virtual ~Btree() {}
-
-   public:
+    //            SIMPLE FUNCTION
+ public:
     int getOrder() { return level; }
+    int rootIsNull() { return root == nullptr; }
 
-   private:
-    bool isEmpty() { return root == nullptr || root->getSizeKeyVal() == 0; }
+ private:
+    const static int MAX_ORDER_OF_NODE = 10000;
+    const static int MIN_ORDER_OF_NODE = 2;
+    int level;
+    NodeBtree<Key, Value>* root;
 };
 }  // namespace s21
--- src/Model/BTree/NodeBTree.h
+++ src/Model/BTree/NodeBTree.h
@@ -2,49 +2,48 @@
 #include <algorithm>
 #include <array>
 #include <memory>
-#include <vector>
 #include <typeinfo>
+#include <vector>
 
 namespace s21 {
-enum class TypeOfNode {
-    ROOT = 0,
-    OTHER = 1,
-    LEAF = 2,
-};
 template <typename Key, typename Value>
 class NodeBtree {
-   private:
-    std::vector<std::shared_ptr<std::pair<Key, Value>>> keyValues;
-    std::vector<std::shared_ptr<NodeBtree<Key, Value>>> descendents;
-    std::weak_ptr<NodeBtree<Key, Value>> parent;
-    size_t level;
-    TypeOfNode type;
-
-   private:
-
-   public:
-    NodeBtree(size_t newLevel, TypeOfNode typeNew) : NodeBtree(newLevel) { type = typeNew; }
-    void setType(TypeOfNode);
-    void setParent(NodeBtree<Key, Value>* parent);
+    //              CONSTRUCTORS AND DESTRUCTOR
+ public:
+    NodeBtree(size_t newLevel, bool typeNew) : NodeBtree(newLevel) { isLeaf = typeNew; }
     NodeBtree(size_t newLevel) : keyValues(0), descendents(0), level(newLevel) {
-        descendents.reserve(level);
-        keyValues.reserve(level - 1);
-        type = TypeOfNode::ROOT;
+        descendents.reserve(2 * level);
+        keyValues.reserve(2 * level - 1);
+        bool = false;
     }
     NodeBtree() = delete;
     NodeBtree(const NodeBtree<Key, Value>&) = delete;
     NodeBtree(NodeBtree<Key, Value>&&) = default;
+
+ public:
+    void setType(bool type) { isLeaf = type; }
     int getLevel() { return level; }
-    TypeOfNode getType() { return type; }
+    bool leaf() { return isLeaf; }
     size_t isCompleted() { return keyValues.size() == level - 1; }
-    size_t getSizeKeyVal(){return keyValues.size();}
-//                 INSERT
-    public:
-    void simpleInsert(const Key& key, const Value& value){
-        auto iter = std::find_if(keyValues.begin(), keyValues.end(),
-                                         [&key](auto i) { return i->first > key; });
-        std::shared_ptr<std::pair<Key, Value>> keyvalue(new std::pair<Key, Value>(key, value));
-        keyValues.insert(iter, keyvalue);
+    size_t getSizeKeyVal() { return keyValues.size(); }
+    //                 INSERT
+ public:
+    void insertForNullRoot(const Key& key, const Value& value) {
+        std::pair<Key, Value>* keyvalue = new std::pair<Key, Value>(key, value);
+        keyValues.insert(keyvalue);
     }
+    void splitDescendents(int index, NodeBtree<Key, Value>* node) {
+        NodeBtree<Key, Value>* tempNode = new NodeBtree<Key, Value>(level, node->isLeaf);
+    }
+    void insert(const Key& key, const Value& value) {}
+    std::pair<Key, Value>* getDescendent(int i) { return keyValues[i]; }
+
+    //          Class Fields
+ private:
+    std::vector<std::pair<Key, Value>*> keyValues;
+    std::vector<NodeBtree<Key, Value>*> descendents;
+
+    size_t level;
+    bool isLeaf;
 };
 }  // namespace s21
